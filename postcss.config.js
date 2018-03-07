d7749440dba0fa49f10c95f12c375d2798119fa9
IvaTsu <ivan.tsud@gmail.com>
2018-03-07 08:32:34 +0200

    refactored server, postCSS and .gitignore configs

--- postcss.config.js
+++ postcss.config.js
@@ -4,15 +4,24 @@
  * для девелоп — названия классов + хэш из 5 символов.
  */
 
+const preCSS = require('precss');
+const autoprefixer = require('autoprefixer');
+const postCSSEasyImport = require('postcss-easy-import');
+const postCSSSimpleVars = require('postcss-simple-vars');
+const postCSSVariables = require('postcss-css-variables');
+const postCSSMixins = require('postcss-mixins');
+const postCSSNested = require('postcss-nested');
+const postCSSColorFunction = require('postcss-color-function');
+
 module.exports = {
   plugins: [
-    require("precss"),
-    require("autoprefixer"),
-    require("postcss-easy-import"),
-    require("postcss-simple-vars"),
-    require("postcss-css-variables"),
-    require("postcss-mixins"),
-    require("postcss-nested"),
-    require("postcss-color-function")
-  ]
+    preCSS,
+    autoprefixer,
+    postCSSEasyImport,
+    postCSSSimpleVars,
+    postCSSVariables,
+    postCSSMixins,
+    postCSSNested,
+    postCSSColorFunction,
+  ],
 };
--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
+
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.babel').default;
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const PORT = 7700;
-const PUBLIC_PATH = `${__dirname}/public`;
+const PUBLIC_PATH = `${__dirname}/src`;
 const app = express();
 
-const isDevelopment = process.env.NODE_ENV === "development";
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 const targetEntry = `http://localhost:${PORT}/`;
 
 if (isDevelopment) {
-  const webpack = require("webpack");
-  const webpackConfig = require("./webpack.config.babel").default;
   const compiler = webpack(webpackConfig);
-  app.use(
-    require("webpack-dev-middleware")(compiler, {
-      hot: true,
-      stats: {
-        colors: true
-      }
-    })
-  );
-  app.use(require("webpack-hot-middleware")(compiler));
+  app.use(webpackDevMiddleware(compiler, {
+    hot: true,
+    stats: {
+      colors: true,
+    },
+  }));
+  app.use(webpackHotMiddleware(compiler));
 } else {
   app.use(express.static(PUBLIC_PATH));
 }
 
-app.all("*", (req, res) => {
-  res.sendFile(path.resolve(PUBLIC_PATH, "index.html"));
+app.all('*', (req, res) => {
+  res.sendFile(path.resolve(PUBLIC_PATH, 'index.html'));
 });
 
 app.listen(PORT, () => {
